4ae7abe920ce1eb7214dd21c2842035759c4a834
Adam Passey <apassey@ign.com>
2012-09-19 11:35:00 -0700

    Created initial Rails skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushCmsRails::Application.config.secret_token = '5856c3ee6609e5e2cf322cb8c733a9167ad2c66554eaa0002c1a55928a594681129b4db313818f469f6f101afab0e65f691c750b915ca26a7f9bea260a5a5fb1'
