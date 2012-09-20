369749b748f8767d4662226fa74e6c5eb526b6cc
Adam Passey <apassey@ign.com>
2012-09-20 11:14:01 -0700

    Added production api endpoint to config, verified notifications send on prd.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,8 +2,10 @@ class NotificationsController < ApplicationController
 
   # post
   def create
-    #app = App.get_by_id params[:id]
-    #@notification = Notification.new app, params[:title], params[:desc]
+    app = App.get_by_id params[:app_id]
+    notification = Notification.new app, { :title => params[:title], :description => params[:description], :URL => params[:URL] }
+    notification.send
+
     redirect_to :controller => "apps", :action => "index", :alert => "Notification sent."
   end
 
--- app/models/Notification.rb
+++ app/models/Notification.rb
@@ -1,9 +1,20 @@
 class Notification
   
-  attr_accessor :title, :description, :url, :orinoco, :myignId
+  include PushapiHelper
+
+  attr_accessor :title, :description, :URL, :orinoco, :myignId, :appId
 
   def initialize(app, options = {})
-    Rails.logger.debug "Creating notification for app: #{app.to_yaml} and options: #{options.to_yaml}"
+    @appId        = app.appleId
+    @title        = options[:title]       if options[:title]
+    @description  = options[:description] if options[:description]
+    @URL          = options[:URL]         if options[:URL]
+    @orinoco      = 0   # this is not coming from orinoco, but needs to send as part of payload
+    @myignId      = 0   # same with myignId
+  end
+
+  def send
+    PushapiHelper.api_post("apps/#{self.appId}/notifications", ActiveSupport::JSON.encode(self))
   end
 
   class << self
